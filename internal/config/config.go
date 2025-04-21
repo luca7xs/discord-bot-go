e72d057334245e09b578ddd3367ec260128631c3
Luca <zluca7xs@gmail.com>
2025-04-21 19:43:46 -0300

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,13 +4,13 @@ import "fmt"
 
 const (
 	// Bot
-	Token   = "MTM1NzUwNDc4ODU4OTQ0NTI4MQ.GvwNsg.l9YsHuXV1kdHvmeHXJ9lCKS4c0bdppO8BBLLlY"
-	GuildID = "1357505318115999935"
+	Token   = "botToken"
+	GuildID = "guildID"
 
 	// Database
 	User     = "root"
-	Password = ""
-	Host     = "192.168.0.2"
+	Password = "password"
+	Host     = "localhost"
 	Port     = "3306"
 	Database = "discord_bot_go"
 )
